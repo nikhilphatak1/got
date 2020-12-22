cfc3c0141bf3868f9247bc4d497097b70f03e98c
Nikhil Phatak <nikhilphatak1@gmail.com>
2020-12-22 17:06:00 -0500

    basic index/add implementation, lots of TODO

--- internal/got/index.go
+++ internal/got/index.go
@@ -6,6 +6,7 @@ import "os"
 type Index struct {
 	entries map[string]interface{}
 	lockfile *Lockfile
+	// TODO hash digest field here
 }
 
 // IndexEntry entry in the index
@@ -16,7 +17,8 @@ type IndexEntry struct {
 
 const (
 	regularMode = 0100644
-	
+	executableMode = 0100755
+	maxPathLen = 0xfff
 )
 
 // NewIndex index constructor
@@ -36,6 +38,58 @@ func (i *Index) Add(targetPath string, oid string, stat os.FileInfo) {
 
 // CreateEntry new entry for this index
 func CreateEntry(targetPath string, oid string, stat os.FileInfo) *IndexEntry {
+	// TODO mode = executableMode if stat is executable else regulatMode
+	// TODO flags = min(len([]byte(targetPath)), maxPathLen)
+	entry := IndexEntry{}
+	entry.ctime = stat.ModTime().Second()
+	entry.ctimeNsec = stat.ModTime().Nanosecond()
+	entry.mtime = stat.ModTime().Second()
+	entry.mtimeNsec = stat.ModTime().Nanosecond()
+	// TODO other fields
+	// also ctime is set to mtime currently
+	// need to figure out how to get create time
+	return &entry
+}
+
+// String convert IndexEntry
+func (e *IndexEntry) String() string {
 	// TODO
-	mode = 
+	return ""
+}
+
+// WriteUpdates write to index
+func (i *Index) WriteUpdates() bool {
+	lock := i.lockfile.HoldForUpdate()
+	if !lock {
+		return false
+	}
+
+	i.beginWrite()
+
+	// TODO create header []byte containing, in order:
+	// "DIRC" string (4 bytes)
+	// "2" (padded to 8 bytes aka 32 bit)
+	// len(i.entries)  (padded to 8 bytes aka 32 bit)
+	// TODO i.write(header)
+
+	for _, entry := range i.entries {
+		i.write(entry.(*IndexEntry).String())
+	}
+	i.finishWrite()
+
+	return true
+}
+
+func (i *Index) beginWrite() {
+	// TODO set SHA digest here
+}
+
+func (i *Index) write(data string) {
+	i.lockfile.Write(data)
+	// TODO update i.digest with data
+}
+
+func (i *Index) finishWrite() {
+	// TODO write digest to lockfile
+	i.lockfile.Commit()
 }
